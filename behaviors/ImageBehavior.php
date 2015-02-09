92afa2b9a5d1fbb45d2950739ba2b74a99faba3a
Dmitry Semenov <disemx@gmail.com>
2015-02-09 16:46:57 +0200

    Allowing custom method name for supporting multiple image path attributes in single model

--- behaviors/ImageBehavior.php
+++ behaviors/ImageBehavior.php
@@ -16,15 +16,61 @@ class ImageBehavior extends Behavior
     public $pathAttribute = 'path';
 
     /**
-     * @param $mode
+     * @var string
+     */
+    public $methodName = 'url';
+
+    /**
+     * @var array
+     */
+    private $_methods = [];
+
+    /**
+     * @attaching method to behavior
+     */
+    public function init()
+    {
+        $this->attachMethod($this->methodName, function ($mode = 'original') {
+            $attribute = $this->owner->{$this->pathAttribute};
+            return Yii::$app->get('image')->getUrl($attribute, $mode);
+        });
+    }
+
+    /**
+     * @param string $name
+     * @param array  $parameters
      *
      * @return mixed
-     * @throws \yii\base\InvalidConfigException
      */
-    public function url($mode = 'original')
+    public function __call($name, $parameters)
     {
-        $attribute = $this->owner->{$this->pathAttribute};
-        return Yii::$app->get('image')->getUrl($attribute, $mode);
+        if (isset($this->_methods[$name])) {
+            return call_user_func_array($this->_methods[$name], $parameters);
+        }
+        return parent::__call($name, $parameters);
     }
+
+    /**
+     * @param string $name
+     *
+     * @return bool
+     */
+    public function hasMethod($name)
+    {
+        if ($name === $this->methodName) {
+            return is_callable([$this, $name]);
+        }
+        return parent::hasMethod($name);
+    }
+
+    /**
+     * @param $name
+     * @param $closure
+     */
+    protected function attachMethod($name, $closure)
+    {
+        $this->_methods[$name] = \Closure::bind($closure, $this);
+    }
+
 
 }
